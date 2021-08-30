20dc23b230d182a41f999b071290012de638bfa2
Chabo17 <cbott001@ucr.edu>
2021-08-30 14:06:51 -0700

    adding loop

--- Bonus.java
+++ Bonus.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Bonus {
     public static void main(String[] args) {
         int n=0;
-
+        boolean boo = true;
         Scanner scan = new Scanner(System.in);
+        while(boo){
         System.out.println("Please enter an integer: ");
         String info = scan.nextLine();
         try{
             n = Integer.parseInt(info);
+            boo = false;
         }
         catch(NumberFormatException ex){
             System.out.println("I said an int");
         }
+    }
         int sum =0;
         int sum2 = 0;
 
@@ -25,5 +28,6 @@ public class Bonus {
             System.out.println(i);
         }
         System.out.println("The sum of all multiples of 3 and 5 to "+n+" is: "+(sum+sum2));
+
     }
 }
